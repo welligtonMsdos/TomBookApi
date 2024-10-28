6a34b6da35bbe28ce05463e8a2f62ce5c5ae0909
Dell <Dell@DESKTOP-TK7GSO1>
2024-10-28 19:05:14 -0300

    chore: ajustes na bookgoogleservice

--- Services/BookGoogleService.cs
+++ Services/BookGoogleService.cs
@@ -8,6 +8,7 @@ public class BookGoogleService : IBook
 {
     private readonly HttpClient _httpClient;
     private const string BASE_PATH = "v1/volumes?q=isbn:";
+    private const string KEY = "&key=AIzaSyDe914FUSV408rptTc14ByoSlB3eSc3hgs";
 
     public BookGoogleService(HttpClient httpClient)
     {
@@ -23,20 +24,20 @@ public class BookGoogleService : IBook
     {
         try
         {
-            var request = await _httpClient.GetAsync(BASE_PATH + isbn);
+            var book = new Book();
+
+            var request = await _httpClient.GetAsync(BASE_PATH + isbn + KEY);
 
             var dataAsString = await request.Content.ReadAsStringAsync().ConfigureAwait(false);
 
             var root = JsonConvert.DeserializeObject<Root>(dataAsString);
-
-            var book = new Book();
 
             book.ISBN = isbn;
             book.Title = root.Items.FirstOrDefault()?.VolumeInfo.Title;
             book.Authors = root.Items.FirstOrDefault()?.VolumeInfo.Authors[0];
             book.Description = root.Items.FirstOrDefault()?.VolumeInfo.Description;
             book.PageCount = root.Items.FirstOrDefault()?.VolumeInfo.PageCount;
-            book.SmallThumbnail = root.Items.FirstOrDefault()?.VolumeInfo.ImageLinks?.smallThumbnail;
+            book.SmallThumbnail = root.Items.FirstOrDefault()?.VolumeInfo.ImageLinks?.smallThumbnail;           
 
             return book;
         }
